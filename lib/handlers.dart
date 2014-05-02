81cbd65b8fc16554ef4e47683929cf13a3797b85
Kevin Moore <github@j832.com>
2014-05-02 12:52:34 -0700

    tweaks

--- lib/handlers.dart
+++ lib/handlers.dart
@@ -1,12 +1,10 @@
 library shelf_html_example.frontend.handlers;
 
 import 'dart:html';
-import 'dart:convert' show JSON;
 
 import 'package:shelf/shelf.dart';
 
 import 'components/app.dart';
-import 'components/sign_in.dart';
 import 'components/card.dart';
 
 part 'src/handlers/sign_in_handler.dart';
